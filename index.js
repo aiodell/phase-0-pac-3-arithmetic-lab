5458cd6cff9c8d4257321bb9e997bca1cb78d8e2
Andrianna <andrianna.odell@gmail.com>
2022-06-06 10:53:49 -0400

    commit tree 4daa5530b5f1e43dd4a1fa68c69e3e4150bc0ebf

---
+++ index.js
@@ -0,0 +1,60 @@
+// define the variables
+a = Math.floor(Math.random() * 1000);
+b = Math.floor(Math.random() * 1000);
+
+// addition functions
+function add(){
+
+}
+
+function add(a,b){
+    return a + b;
+}
+
+// subtraction functions
+function subtract(){
+
+}
+function subtract(a,b){
+    return a - b;
+}
+
+// multiplication functions
+function multiply(){
+
+}
+function multiply(a,b){
+    return a * b;
+}
+
+// division functions
+function divide(){
+
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+// increment and decrement functions
+
+function increment(a){
+    return (a + 1);
+}
+
+function decrement(a){
+    return (a - 1);
+}
+
+// integer conversion equations
+function makeInt(a){
+    return a.toString();
+}
+
+function makeInt(a){
+    return parseInt(a, 10);
+}
+
+function preserveDecimal(a){
+    return parseFloat(a);
+}
